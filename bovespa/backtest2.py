267d3455ff1c19877f16a013678fb4396bdbd900
Victor <victorcorcos@gmail.com>
2020-08-29 04:36:04 -0300

    Add backtest.py backtesting - This one is amazing!

---
+++ bovespa/backtest2.py
@@ -0,0 +1,55 @@
+# Automatically Execute Backtest with the Bovespa shares with the provided period and tickers
+
+# Quick Tutorial: https://www.youtube.com/watch?v=d2qrsCfXung&t=17s
+
+# How to use this file...
+#
+# python3
+#
+# import sys, os
+# sys.path.extend([f'./{name}' for name in os.listdir(".") if os.path.isdir(name)])
+# import backtest2
+# 
+# backtest2.backtest(period='5y', tickers=['ABEV3', 'EGIE3', 'WEGE3', 'ITUB3', 'MDIA3', 'GRND3', 'ODPV3', 'ENBR3', 'PSSA3', 'FLRY3'])
+
+import yfinance as yf
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import pyfolio as pf
+import warnings
+warnings.filterwarnings('ignore')
+
+import click
+
+# Execute the backtest for the provided period and tickers
+# backtest(period='5y', tickers=['ABEV3', 'EGIE3', 'WEGE3', 'ITUB3', 'MDIA3', 'GRND3', 'ODPV3', 'ENBR3', 'PSSA3', 'FLRY3'])
+def backtest(period, tickers):
+  tickers = list(map(lambda t: t + '.SA', tickers)) # Add '.SA' on the ending of the tickers
+  tickers += ['^BVSP'] # Add Ibovespa index to tickers
+
+  # Bring the daily tickers prices from the provided period until today
+  dados_yahoo = yf.download(tickers=tickers, period=period)['Adj Close']
+
+  # Calculate the percentage of daily return
+  retorno = dados_yahoo.pct_change()
+  retorno.iloc[0] = 0
+
+  # Calculate the percentage of accumulated return
+  retorno_acumulado = (1 + retorno).cumprod()
+
+  # Execute the Backtest investing R$1.000,00 on each ticker
+  carteira = 1000 * retorno_acumulado.iloc[:, :10] # Retira a coluna do Bovespa
+  carteira['saldo'] = carteira.sum(axis=1)
+  carteira['retorno'] = carteira['saldo'].pct_change()
+
+  # Bring how much money would you have today
+  montante = carteira.tail(1)['saldo'][0]
+  click.secho(f"Montante Final: {montante}", fg='blue', bold=True)
+  click.secho(f"Valorização: {'{0:.0%}'.format(montante / 10000)}", fg='red', bold=True)
+  return montante
+
+  # # Beautifully plots the result on the screen
+  # pf.create_full_tear_sheet(carteira['retorno'], benchmark_rets=retorno['^BVSP'])
