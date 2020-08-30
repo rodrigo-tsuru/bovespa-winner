ac589cade3d7fb113300194b2c3748d101d9ae44
Victor <victorcorcos@gmail.com>
2020-08-30 09:08:35 -0300

    Add backtests shell script

---
+++ shell_scripts/automate_backests.sh
@@ -0,0 +1,14 @@
+# Run backtests on Brazilian Bovespa Stocks
+run_backtests() {
+  ttab {cd /Users/victor/Desktop/python/bovespa-winner; python3 graham.py '{ "year": 2008 }'}
+  ttab {cd /Users/victor/Desktop/python/bovespa-winner; python3 graham.py '{ "year": 2009 }'}
+  ttab {cd /Users/victor/Desktop/python/bovespa-winner; python3 graham.py '{ "year": 2010 }'}
+  ttab {cd /Users/victor/Desktop/python/bovespa-winner; python3 graham.py '{ "year": 2011 }'}
+  ttab {cd /Users/victor/Desktop/python/bovespa-winner; python3 graham.py '{ "year": 2012 }'}
+  ttab {cd /Users/victor/Desktop/python/bovespa-winner; python3 graham.py '{ "year": 2013 }'}
+  ttab {cd /Users/victor/Desktop/python/bovespa-winner; python3 graham.py '{ "year": 2014 }'}
+  ttab {cd /Users/victor/Desktop/python/bovespa-winner; python3 graham.py '{ "year": 2015 }'}
+  ttab {cd /Users/victor/Desktop/python/bovespa-winner; python3 graham.py '{ "year": 2016 }'}
+  ttab {cd /Users/victor/Desktop/python/bovespa-winner; python3 graham.py '{ "year": 2017 }'}
+  ttab {cd /Users/victor/Desktop/python/bovespa-winner; python3 graham.py '{ "year": 2018 }'}
+}
